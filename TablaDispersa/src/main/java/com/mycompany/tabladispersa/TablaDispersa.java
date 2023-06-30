e5546b63b51e500ab29ddec6f166358dbbc4750d
Thomasvg02 <127699585+Thomasvg02@users.noreply.github.com>
2023-06-30 07:03:48 -0300

    Add files via upload

---
+++ TablaDispersa/src/main/java/com/mycompany/tabladispersa/TablaDispersa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tabladispersa;
+
+/**
+ *
+ * @author valen
+ */
+public class TablaDispersa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
